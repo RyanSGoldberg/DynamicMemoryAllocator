ab3ed9f981a51789d41378e6fb2f074d2eec1546
Ryan Goldberg <ryan.s.goldberg@gmail.com>
2021-04-24 12:31:26 -0400

    Added small testing suite

--- Makefile
+++ Makefile
@@ -2,13 +2,21 @@ CFLAGS = -g -Wall -std=gnu99
 
 all: allocator_test
 
+verbose: allocator_test.o allocator.o
+		gcc ${CFLAGS} -DVERBOSE=1  -o $@ $^	
+
 allocator_test: allocator_test.o allocator.o
 		gcc ${CFLAGS} -o $@ $^
 
 %.o: %.c allocator.h
 		gcc ${CFLAGS} -c $<
 
-.PHONY: clean all
+.PHONY: clean all tests
 
 clean:
 		rm -f *.o allocator_test
+
+tests: allocator_test
+		./allocator_test > output.txt;                     \
+		diff correct_output.txt output.txt > /dev/null;    \
+		if [ $$? -eq 0 ]; then echo "Tests Passed"; else diff -y --suppress-common-lines correct_output.txt output.txt || exit 0; fi  		
--- allocator_test.c
+++ allocator_test.c
@@ -3,26 +3,43 @@
 #include "allocator.h"
 
 #define a "HelloWorld"
-#define b "FooBar"
+#define b "FooBar!"
 #define c "ShalomOlam"
+#define d '5'
 
 
 
 int main(int argc, char **argv){
+    printf("Beginnings Tests\n");
+    // Fill the heap with back to back elements
     char *x = cust_malloc(strlen(a)+1);
     strncpy(x, a, strlen(a)+1);
-    printf("%s\n", x);
     char *y = cust_malloc(strlen(b)+1);
     strncpy(y, b, strlen(b)+1);
-    printf("%s\n", y);
     char *z = cust_malloc(strlen(c)+1);
     strncpy(z, c, strlen(c)+1);
-    printf("%s\n", z);
     // Exactly 128 bytes are on the heap rn
     char *i = cust_malloc(1);
     *i = '5';
-    printf("%s\n", i);
-
+    // Free the last element and make sure the block size decreases then put it back
+     cust_free(i);
+    heapdump();
+    i = cust_malloc(1);
+    *i = '5';
+    // Free the 'middle' element and try put something too big into that slot
+    cust_free(y);
+    y = cust_malloc(strlen("foobar!1")+1);
+    strncpy(y, "foobar!1", strlen("foobar!1")+1);
+    heapdump();
+    // Remove the first element and make sure something new will fill that slot if it fits
     cust_free(x);
+    x = cust_malloc(strlen(b)+1);
+    strncpy(x, b, strlen(b)+1);
+    heapdump();
+    // Freeing the last element left will free up everything
+    cust_free(x);
+    cust_free(z);
+    cust_free(i);
+    cust_free(y);
     heapdump();
 }
